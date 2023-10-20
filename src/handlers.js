6ee606c6a5d295fc01eb99e8a41d823f27d76570
JacksoonHayes <jackson-hayes@live.nmit.ac.nz>
2023-10-20 13:17:48 +1300

    session token obtaining

--- src/handlers.js
+++ src/handlers.js
@@ -7,12 +7,23 @@ const jwtExpirySeconds = 300;
 
 // define a user database including username and password
 
+const users = {
+    user1: 'password1',
+    user2: 'password2',
+    user3: 'password3'
+}
+
 const signIn = (req, res) => {
+
 // gets credentials from json body
     const { username, password } = req.body;
     if (!username || !password || users[username] !== password) {
+
+    // return a 401 error if username or password is not valid
+    // or if password does not match the username
         return res.status(401).end();
     }
+
     const token = jwt.sign({ username }, jwtKey, {
         algorithm: 'HS256',
         expiresIn: jwtExpirySeconds,
@@ -20,4 +31,13 @@ const signIn = (req, res) => {
     res.cookie('token', token, { maxAge: jwtExpirySeconds * 1000 });
     res.end();
 
+    // obtain the session token from the requests cookie
+    const access = (req, res) => {
+        const token = req.cookies.token;
+
+        // if the cookie is not set, return an unauthorized error
+        if (!token) {
+            return res.status(401).end();
+        }
+    }
 };
